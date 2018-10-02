d564a1a26ae3bdb6887422b29a592e64f21b926b
Peter Bassett <PeterBassett@synectics-solutions.com>
2018-10-02 17:12:10 +0100

    added thr rest of the actual code

---
+++ Anagram/LetterTree.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using Combinatorics;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using Combinatorics.Collections;
+
+namespace Anagram
+{
+    public class LetterNode
+    {
+        public static int MaxDepthSearched = 0;
+
+        public char Letter;
+        public Dictionary<char, LetterNode> Children;
+        public bool IsCompleteWord = false;
+        public bool WordEnumerated = false;
+
+        public LetterNode()
+        {
+            Letter = char.MaxValue;
+        }
+
+        public LetterNode(char cLetter)
+        {
+            Letter = cLetter;
+        }
+
+        public void StoreMaxDepth(int iDepth)
+        {
+            if (MaxDepthSearched < iDepth)
+                MaxDepthSearched = iDepth;
+        }
+
+        public bool IsTerminator
+        {
+            get
+            {
+                if (Children == null)
+                    return true;
+
+                if (Children.Count == 0)
+                    return true;
+
+                return false;
+            }
+        }
+
+        public void AddWord(string strWord, LetterTree parent)
+        {
+            if (strWord.Length == 0)
+                return;
+
+            char cLetter = strWord[0];
+            
+            LetterNode oChild = null;
+
+            if (Children == null)
+                Children = new Dictionary<char,LetterNode>();
+
+            if (!Children.ContainsKey(cLetter))
+            {
+                oChild = new LetterNode(cLetter);
+                parent.LetterNodes.Add(oChild);
+                Children.Add(cLetter, oChild);
+            }
+            else
+                oChild = Children[cLetter];
+
+            if (strWord.Length == 1)
+                oChild.IsCompleteWord = true;
+
+            oChild.AddWord(strWord.Substring(1), parent);
+        }
+
+        public IEnumerable<string> Search(string strSearch, int iDepth)
+        {
+            StoreMaxDepth(iDepth);
+
+            if (IsTerminator && Letter == strSearch[0] && !this.WordEnumerated)
+            {
+                this.WordEnumerated = true;
+                yield return strSearch[0].ToString();
+            }
+
+            if (Letter != strSearch[0] && this.Letter != char.MaxValue)
+                yield break;
+
+            if(Letter == strSearch[0] && strSearch.Length == 1 && this.IsCompleteWord == true && !this.WordEnumerated)
+            {
+                this.WordEnumerated = true;
+                yield return strSearch[0].ToString();
+            }
+
+            string strNewSearch = strSearch;
+
+            if (this.Letter != char.MaxValue)
+                strNewSearch = strSearch.Substring(1);
+
+            if (strNewSearch.Length == 0)
+                yield break;
+
+            if (IsTerminator)
+                yield break;
+
+            if (Children == null || !Children.ContainsKey(strNewSearch[0]))
+                yield break;
+
+            foreach (var strSubword in Children[strNewSearch[0]].Search(strNewSearch, iDepth + 1))
+                yield return this.Letter + strSubword;
+        }
+
+        internal void ResetEnumeration()
+        {
+            MaxDepthSearched = 0;
+            this.WordEnumerated = false;
+
+            if (Children != null)
+            {
+                foreach (var child in Children.Values)
+                {
+                    child.ResetEnumeration();
+                }
+            }
+        }        
+    }
+
+    public class LetterTree
+    {
+        public LetterNode TreeRoot = new LetterNode(char.MaxValue);
+        public List<LetterNode> LetterNodes = new List<LetterNode>();
+        private BlockingCollection<string> m_data = null;
+
+        public void AddWord(string strWord)
+        {
+            strWord = strWord.ToLower();
+            TreeRoot.AddWord(strWord, this);            
+        }
+
+        private class ThreadParams
+        {
+            public string Anagram { get; set; }
+            public Action<string> Progress { get; set; }
+        }
+
+        private void ResetEnumeration()
+        {
+            Parallel.ForEach(this.LetterNodes, (node) =>
+                {
+                    node.ResetEnumeration();
+                });
+        }
+        
+        public IEnumerable<string> Search(string strAnagram, Action<string> Progress)
+        {
+            m_data = new BlockingCollection<string>();
+
+            ResetEnumeration();
+
+            // start the enumeration task
+            Task.Factory.StartNew(SearchThread,
+                    new ThreadParams()
+                    {
+                        Anagram = strAnagram,
+                        Progress = Progress
+                    });
+
+            return m_data.GetConsumingEnumerable();
+        }
+
+        private void SearchThread(object threadParam)
+        {
+            ThreadParams oParams = (ThreadParams)threadParam;
+
+            /// from nuget package https://www.nuget.org/packages/Combinatorics/
+            var permutations = new Permutations<char>(oParams.Anagram.ToLower().ToCharArray(), GenerateOption.WithoutRepetition);
+            
+            // parallel iteration of the permutation list
+            Parallel.ForEach(permutations, (IList<char> chars) => {                    
+                string strWord = new string(chars.ToArray());
+
+                oParams.Progress?.Invoke(strWord);
+
+                // add each work to the blocking collection
+                foreach (var strFoundWord in TreeRoot.Search(strWord, 0))
+                    m_data.Add(strFoundWord.Substring(1));
+            });
+
+            //Adding complete, the enumeration can stop now
+            m_data.CompleteAdding();
+        }
+    }
+}
---
+++ Anagram/Program.cs
@@ -0,0 +1,596 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Collections;
+using Combinatorics;
+using Combinatorics.Collections;
+
+namespace Anagram
+{
+    class Program
+    {
+        public enum Mode
+        {
+            None,
+            AnagramSolver,
+            SimilarWord,
+            CrosswordAnagram // to solve clues given in an android game my wife has
+        }
+
+        static void Main(string[] args)
+        {
+            Mode mode;
+            do
+            {
+                mode = GetExecutionMode();
+
+                RunMode(mode);
+            } while (mode != Mode.None);
+        }
+
+        private static void RunMode(Mode mode)
+        {
+            if (mode == Mode.None)
+                return;
+
+            var wordslist = LoadWordFrequencies();
+
+            switch (mode)
+            {
+                case Mode.AnagramSolver:
+                    {
+                        var tree = BuildTrie(wordslist.Keys.Cast<string>());
+                        AnagramSolver(tree);
+                        break;
+                    }
+                case Mode.SimilarWord:
+                    {
+                        SimilarWords(new HashSet<string>(wordslist.Keys.Cast<string>()));
+                        break;
+                    }
+                case Mode.CrosswordAnagram:
+                    {
+
+                        CrosswordAnagramSolver(wordslist);
+                        break;
+                    }
+                default:
+                    break;
+            }
+        }
+
+        private static Mode GetExecutionMode()
+        {
+            Console.WriteLine("1 = Anagram Solver");
+            Console.WriteLine("2 = Crossword Anagram Solver");
+            Console.WriteLine("3 = Similar Words");
+            Console.WriteLine("Press any other key to exit");
+
+            var answer = Console.ReadKey();
+
+            Console.WriteLine("");
+
+            if (answer.KeyChar == '1')
+                return Mode.AnagramSolver;
+            else if (answer.KeyChar == '2')
+                return Mode.CrosswordAnagram;
+            else if (answer.KeyChar == '3')
+                return Mode.SimilarWord;
+            else
+                return Mode.None;
+        }
+
+        private static LetterTree BuildTrie(IEnumerable<string> words)
+        {
+            Console.WriteLine("Populating Letter Tree");
+
+            LetterTree root = new LetterTree();
+
+            foreach (string word in words)
+                root.AddWord(word);
+
+            return root;
+        }
+
+        private static void AnagramSolver(LetterTree root)
+        {
+            do
+            {
+                Console.WriteLine("----------------------------------------------------------------------------");
+                Console.WriteLine("Enter anagram to solve. Type EXIT to exit back to the main menu.");
+                string data = Console.ReadLine();
+
+                if (data.Trim() == "EXIT")
+                    break;
+
+                if (data.Trim().Length == 0)
+                    continue;
+
+                int iX = Console.CursorLeft;
+                int iY = Console.CursorTop;
+                var iCount = 0;
+                var lockable = new object();
+                Action<string> SearchProgress = (word) =>
+                {
+                    iCount++;
+
+                    if (iCount % 5000 == 0)
+                    {
+                        lock (lockable)
+                        {
+                            Console.CursorLeft = iX;
+                            Console.CursorTop = iY;
+
+                            Console.WriteLine(word.PadRight(40));
+                        }
+                    }
+                };
+
+                var oOutput = from s in root.Search(data, SearchProgress)
+                              let score = ScrabbleWordScore(s)
+                              //where score > 10 || s.Length > 6
+                              orderby s.Length descending
+                              select s;
+
+                oOutput = oOutput.ToArray();
+
+                Console.WriteLine("ANSWERS!");
+                Console.WriteLine("-------------------------------------------");
+                foreach (var strWord in oOutput)
+                {
+                    Console.WriteLine("{0}", strWord);
+                }
+
+            } while (true);
+        }
+
+        private static void CrosswordAnagramSolver(Dictionary<string, int> wordList)
+        {
+            do
+            {
+                Console.WriteLine("----------------------------------------------------------------------------");
+                Console.WriteLine("Enter clue. '?' for missing letters and space to separate words. Type EXIT to exit back to the main menu.");
+                string clue = Console.ReadLine();
+                clue = clue.Trim();
+                if (clue == "EXIT")
+                    break;
+
+                Console.WriteLine("Enter the set of available letters. Type EXIT to exit back to the main menu");
+                string availableLetters = Console.ReadLine();
+                availableLetters = availableLetters.Trim();
+                if (availableLetters == "EXIT")
+                    break;
+
+                if (clue.Length == 0 || availableLetters.Length == 0)
+                    continue;
+
+                int iX = Console.CursorLeft;
+                int iY = Console.CursorTop;
+                var iCount = 0;
+                var lockable = new object();
+                Action<string> SearchProgress = (word) =>
+                {
+                    iCount++;
+
+                    if (iCount % 5000 == 0)
+                    {
+                        lock (lockable)
+                        {
+                            Console.CursorLeft = iX;
+                            Console.CursorTop = iY;
+
+                            Console.WriteLine(word.PadRight(40));
+                        }
+                    }
+                };
+
+                var words = clue.Split(new[] { ' ' });
+
+                var permutations = new Permutations<char>(availableLetters.ToLower().ToCharArray(), GenerateOption.WithoutRepetition);
+
+                Console.WriteLine("Searching {0:n0} permutations", permutations.Count);
+
+                var answerSet = new Dictionary<string, int>();
+                foreach (IList<char> p in permutations)
+                {
+                    var permutation = new Queue<char>(p);
+
+                    var foundWords = new List<Tuple<string, int>>();
+                    bool foundAll = true;
+                    foreach (var word in words)
+                    {
+                        var arr = word.ToCharArray();
+
+                        for (int i = 0; i < arr.Length; i++)
+                        {
+                            if (arr[i] == '?')
+                                arr[i] = permutation.Dequeue();
+                        }
+
+                        var testWord = new string(arr);
+                        if (wordList.ContainsKey(testWord))
+                        {
+                            foundWords.Add(new Tuple<string, int>(testWord, wordList[testWord]));
+                        }
+                        else
+                        {
+                            foundAll = false;
+                            break;
+                        }
+                    }
+
+                    if (foundAll)
+                    {
+                        var answer = string.Join(" ", foundWords.Select(w => w.Item1).ToArray());
+                        var frequency = foundWords.Sum(w => w.Item2);
+
+                        if (answerSet.ContainsKey(answer))
+                            continue;
+
+                        answerSet.Add(answer, frequency);
+
+                        //Console.WriteLine("FOUND - " + answer);
+                    }
+                }
+
+                var orderedAnswers = answerSet.OrderByDescending(a => a.Value);
+
+                foreach (var answer in orderedAnswers)
+                {
+                    Console.WriteLine("FOUND - " + answer.Key + "\t\t\t" + answer.Value);
+                }
+
+            } while (true);
+        }
+
+        private static Dictionary<string, int> LoadWordFrequencies()
+        {
+            //if (!File.Exists(@"AllWords.zip"))
+            // the source file for this was deleted as it took up too much space.
+            //    BuildWordFrequencyFile();
+
+            var archive = System.IO.Compression.ZipFile.OpenRead(@"AllWords.zip");
+
+            var entry = archive.GetEntry("AllWords.txt");
+
+            var linesFromFile = new List<string>();
+            using (var reader = new StreamReader(entry.Open()))
+            {
+                while (!reader.EndOfStream)
+                    linesFromFile.Add(reader.ReadLine());
+            }
+
+            return (from line in linesFromFile
+                    let words = line.Split('\t')
+                    select new Tuple<string, int>(words[0], int.Parse(words[1]))).ToDictionary(t => t.Item1, t => t.Item2);
+        }
+       
+        private static void SimilarWords(HashSet<string> wordslist)
+        {
+            var candidateStrings = new List<string>();
+            string entry;
+
+            while (true)
+            {
+                Console.WriteLine("Enter a word to examine. Blank to skip");
+                entry = Console.ReadLine();
+
+                if (entry.Length == 0)
+                    break;
+
+                candidateStrings.Add(entry);
+            }
+
+            var candidates = candidateStrings.ToDictionary(c => c, c => new SortedList<int, List<string>>());
+
+            foreach (var candidate in wordslist)
+            {
+                foreach (var target in candidates.Keys)
+                {
+                    var distance = Distance(target, candidate);
+
+                    if (distance > 3)
+                        continue;
+
+                    var list = candidates[target];
+
+                    if (!list.ContainsKey(distance))
+                        list.Add(distance, new List<string>());
+
+                    list[distance].Add(candidate);
+
+                    if (list.Count > 10)
+                        list.RemoveAt(list.Count - 1);
+                }
+            }
+
+            foreach (var target in candidates.Keys)
+            {
+                Console.WriteLine(target);
+                var words = candidates[target];
+                foreach (var score in words.Keys)
+                {
+                    Console.WriteLine("\tScore {0}", score);
+                    foreach (var word in words[score])
+                    {
+                        Console.WriteLine("\t\t{0}", word);
+                    }
+                }
+            }
+        }
+
+        private static int Distance(string a, string b)
+        {
+            int aLength = a.Length;
+            int bLength = b.Length;
+
+            int[,] distances = new int[aLength + 1, bLength + 1];
+
+            if (aLength == 0)
+                return bLength;
+
+            if (bLength == 0)
+                return aLength;
+
+            for (int i = 0; i <= aLength; i++)
+                distances[i, 0] = i;
+
+            for (int j = 0; j <= bLength; j++)
+                distances[0, j] = j;
+
+            for (int i = 1; i <= aLength; i++)
+            {
+                for (int j = 1; j <= bLength; j++)
+                {
+                    int cost = (b[j - 1] == a[i - 1]) ? 0 : 1;
+
+                    distances[i, j] = Math.Min(
+                                        Math.Min(
+                                            distances[i - 1, j] + 1,
+                                            distances[i, j - 1] + 1
+                                        ),
+                                        distances[i - 1, j - 1] + cost
+                                     );
+                }
+            }
+
+            return distances[aLength, bLength];
+        }
+
+        private static int ScrabbleWordScore(string strWord)
+        {
+            return (from l in strWord.ToUpper().ToCharArray()
+                    select ScrabbleLetterScore(l)).Sum();
+        }
+
+        private static int ScrabbleLetterScore(char letter)
+        {
+            /*
+               1 point: E ×12, A ×9, I ×9, O ×8, N ×6, R ×6, T ×6, L ×4, S ×4, U ×4
+               2 points: D ×4, G ×3
+               3 points: B ×2, C ×2, M ×2, P ×2
+               4 points: F ×2, H ×2, V ×2, W ×2, Y ×2
+               5 points: K ×1
+               8 points: J ×1, X ×1
+               10 points: Q ×1, Z ×1
+            */
+
+            switch (letter)
+            {
+                case 'E':
+                case 'A':
+                case 'I':
+                case 'O':
+                case 'N':
+                case 'R':
+                case 'T':
+                case 'L':
+                case 'S':
+                case 'U':
+                    return 1;
+                case 'D':
+                case 'G':
+                    return 2;
+                case 'B':
+                case 'C':
+                case 'M':
+                case 'P':
+                    return 3;
+                case 'F':
+                case 'H':
+                case 'V':
+                case 'W':
+                case 'Y':
+                    return 4;
+                case 'K':
+                    return 5;
+                case 'J':
+                case 'X':
+                    return 8;
+                case 'Q':
+                case 'Z':
+                    return 10;
+                default:
+                    throw new ApplicationException("Unaccounted letter " + letter.ToString()); ;
+            }
+        }
+
+        /*
+        #region unused functionality
+        // These functions were used in the building of the AllWords.zip file.
+
+        private static void BuildWordFrequencyFile()
+        {
+            var wordList = LoadWordList();
+            var frequencies = ProcessFrequencies();
+
+            var wordFrequencies = from word in wordList
+                                  select new
+                                  {
+                                      Word = word,
+                                      Frequency = frequencies.ContainsKey(word) ? frequencies[word] : -1
+                                  };
+
+            var lines = from word in wordFrequencies
+                        select string.Format("{0}\t{1}", word.Word, word.Frequency);
+
+            File.WriteAllLines("AllWords.txt", lines.ToArray());
+
+            using (var fs = new FileStream("AllWords.zip", FileMode.Create))
+            using (var archive = new System.IO.Compression.ZipArchive(fs, System.IO.Compression.ZipArchiveMode.Create))
+            {
+                var entry = archive.CreateEntry("AllWords.txt");
+                using (var output = entry.Open())
+                using (var input = File.Open("AllWords.txt", FileMode.Open, FileAccess.Read))
+                    input.CopyTo(output);
+            }
+
+            Console.WriteLine("{0} words loaded", lines.Count());
+        }
+
+        static HashSet<string> LoadWordsFromArchive(Action<bool, string> Progress)
+        {
+            string cleanse(string data)
+            {
+                return data.ToLower().Trim();
+            }
+
+            var words = new HashSet<string>();
+
+            using (var archive = System.IO.Compression.ZipFile.OpenRead("RawWords.zip"))
+            {
+                foreach (var entry in archive.Entries)
+                {
+                    using (var stream = new StreamReader(entry.Open()))
+                    {
+                        while (!stream.EndOfStream)
+                        {
+                            var word = stream.ReadLine();
+
+                            var cleansedWord = cleanse(word);
+
+                            if (string.IsNullOrEmpty(cleansedWord))
+                                continue;
+
+                            if (!words.Contains(cleansedWord))
+                            {
+                                Progress?.Invoke(true, cleansedWord);
+
+                                words.Add(cleansedWord);
+                            }
+                        }
+                    }
+                }
+            }
+            return words;
+        }
+
+
+        private static HashSet<string> LoadWordList()
+        {
+            Console.Write("Loading words... ");
+
+            int iX = Console.CursorLeft;
+            int iY = Console.CursorTop;
+            int iCount = 0;
+            Action<bool, string> LoadProgress = (include, word) =>
+            {
+                iCount++;
+
+                if (iCount % 1000 == 0)
+                {
+                    Console.CursorLeft = iX;
+                    Console.CursorTop = iY;
+
+                    if (include)
+                        Console.WriteLine(word.PadRight(40));
+                }
+            };
+
+            HashSet<string> words = LoadWordsFromArchive(LoadProgress);
+
+            iCount = -1;
+            LoadProgress(true, "");
+
+            Console.WriteLine("{0} words loaded", words.Count);
+
+            return words;
+        }
+
+        private static Dictionary<string, int> ProcessFrequencies()
+        {
+            var lines = File.ReadAllLines(@"..\..\1_1_all_fullalpha.txt");
+
+            bool shouldIgnore(string word)
+            {
+                if (word.Contains("-"))
+                    return true;
+
+                if (word.Contains("/"))
+                    return true;
+
+                if (word.Contains("*"))
+                    return true;
+
+                if (word.Contains("+"))
+                    return true;
+
+                if (word.Contains("."))
+                    return true;
+
+                if (word.Contains("&"))
+                    return true;
+
+                return false;
+            }
+
+            var words = new Dictionary<string, int>();
+
+            foreach (var line in lines)
+            {
+                var parts = line.Split('\t');
+
+                var frequency = int.Parse(parts[4]);
+
+                if (frequency == 0)
+                    continue;
+
+                var word = parts[1];
+
+                if (shouldIgnore(parts[1]))
+                    continue;
+
+                if (parts[1] == "@")
+                {
+                    if (shouldIgnore(parts[3]))
+                        continue;
+
+                    word = parts[3];
+                }
+
+                if (word.StartsWith("'") || word.EndsWith("'"))
+                    continue;
+
+                if (word.Any((c) => !char.IsLetter(c)))
+                    continue;
+
+                word = word.ToLower();
+
+                if (!words.ContainsKey(word))
+                    words.Add(word, frequency);
+                else
+                {
+                    var currentFrequency = words[word];
+
+                    if (currentFrequency < frequency)
+                        words[word] = frequency;
+                }
+            }
+
+            return words;
+        }
+        #endregion*/
+    }
+}
